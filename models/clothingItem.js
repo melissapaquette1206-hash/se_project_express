3f7b84695d443daaddd24a258450b8caf17602e4
Melissa Paquette <melissapaquette1206@gmail.com>
2026-03-24 19:39:15 -0400

    initial sprint 12 commit

---
+++ models/clothingItem.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const clothingItemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  weather: {
+    type: String,
+    required: true,
+  },
+  imageURL: {
+    type: String,
+    required: true,
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: "Link is not valid",
+    },
+  },
+});
+
+module.exports = mongoose.model("item", clothingItemSchema);

0dbfde223ec27af083a9baa20f73728271b31bfe
Melissa Paquette <melissapaquette1206@gmail.com>
2026-04-13 14:54:13 -0400

    fixed issues in routes folder

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -1,13 +1,17 @@
 const router = require("express").Router();
 const auth = require("../middlewares/auth");
 
+const {
+  getItems,
+  createItem,
+  deleteItem,
+  likeItem,
+  dislikeItem,
+} = require("../controllers/clothingItems");
+
 router.get("/", getItems);
-
 router.use(auth);
-
 router.post("/", createItem);
 router.delete("/:itemId", deleteItem);
 router.put("/:itemId/likes", likeItem);
 router.delete("/:itemId/likes", dislikeItem);
-
-module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
 const auth = require("../middlewares/auth");
 
+const { createUser, login } = require("../controllers/users");
+const { getItems } = require("../controllers/clothingItems");
+const usersRouter = require("./users");
+
 router.post("/signup", createUser);
 router.post("/signin", login);
 router.get("/items", getItems);
 
 router.use(auth);
-
 router.use("/users", usersRouter);
-router.use("/items", protectedItemsRouter);
-
-module.exports = router;

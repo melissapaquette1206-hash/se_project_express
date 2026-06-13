d89e0b5e7d7b998fb314f2786ca68de810ea1c79
Melissa Paquette <melissapaquette1206@gmail.com>
2026-06-13 12:11:00 -0400

    updates

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const NotFoundError = require("./errors/NotFoundError");
+const NotFoundError = require("../errors/NotFoundError");
 
 const { validateSignup, validateSignin } = require("../middlewares/validation");
 
